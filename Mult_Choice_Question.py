54ab1fa08867b94645e7682d5c7695e19dd0f552
Dave <37153602+Eulergroupie@users.noreply.github.com>
2020-06-29 15:31:09 -0600

    first commit

---
+++ Mult_Choice_Question.py
@@ -0,0 +1,8 @@
+# Mult_Choice_Question.py
+# Dave: develop a format for multiple choice questions here
+# The goal is to study in a way that I won't forget basics
+
+class Mult_Choice_Question:
+
+    def __init__(self):
+        pass
